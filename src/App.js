fc68cbf036e9571b1356b3bd5870cc3e208e1374
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-10-23 05:01:06 +0500

    sleeping

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import 'antd/dist/reset.css'
-
+import 'antd/dist/reset.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import Home from './pages/About';
+import About from './pages/Contact';
+import Contact from './pages/Home';
 
 function App() {
   return (
