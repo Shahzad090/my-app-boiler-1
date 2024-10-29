2453ea1f9e612446e3f7d52da82fd90d16bd5422
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-10-29 23:56:58 +0500

    .

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} /> 
         <Route path="/contact" element={<Contact />} /> 
-        <Route path="/contact" element={<Contact />} /> 
+        
       </Routes>
     </Router>
   );
