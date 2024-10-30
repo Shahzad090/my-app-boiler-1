e61aa033da103f2bf526482c2ce511eb0f2b28fc
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-10-30 20:52:47 +0500

    clas

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
-const Contact = () => {
+function Contact() {
+  const navigate = useNavigate();
+
+  const handleHomeButtonClick = () => {
+    navigate('/'); 
+  };
   return (
-    <div>
-      <h1>Welcome to Contact page1</h1>
-    </div>
-  );
+    <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Contact Us</h1>
+        <Button type="primary" onClick={handleHomeButtonClick}>
+          Go to Home Page
+        </Button>
+      </div>
+      );
 }
 
 export default Contact;
