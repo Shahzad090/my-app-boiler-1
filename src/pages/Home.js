cf4fbf1171ba50d669b68a6ae0eb3498276115c2
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-11-01 23:30:24 +0500

    starting work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
           Go To About Us
         </Button>
         <Button type="primary" onClick={handleContactClick}>
-          Go To Contact Us
+          Go To Contact U
         </Button>
       </div>
     </div>
